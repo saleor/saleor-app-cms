6ce357aeeec2fe8c2f28e02a25295c9182a69b83
Adrian Pilarczyk <adrianpilarczyk314@gmail.com>
2022-12-06 20:30:18 +0100

    refactor: :recycle: replace form components with mui / macaw

--- src/components/ConfigurationForm.tsx
+++ src/components/ConfigurationForm.tsx
@@ -1,7 +1,18 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Checkbox,
+  FormControlLabel,
+  Grid,
+  TextField,
+} from "@material-ui/core";
+import { ExpandMore as ExpandMoreIcon } from "@material-ui/icons";
+import { Button } from "@saleor/macaw-ui";
 import React from "react";
 import { Path, useForm } from "react-hook-form";
-import { cmsProviders, ProvidersSchema, CMSProvider, providersConfig } from "../lib/cms";
+import { CMSProvider, cmsProviders, providersConfig, ProvidersSchema } from "../lib/cms";
 
 type ConfigurationFormProps<TProvider extends CMSProvider> = {
   defaultValues: ProvidersSchema[TProvider];
@@ -35,30 +46,37 @@ export const ConfigurationForm = <TProvider extends CMSProvider>({
 
   return (
     <form onSubmit={handleSubmit(submitHandler)}>
-      <details>
-        <summary>
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <span>{providersConfig[provider].label}</span>
-        </summary>
-        <label>
-          On / off
-          <input {...register("enabled" as Path<ProvidersSchema[TProvider]>)} type="checkbox" />
-        </label>
-        {fields.map((tokenName) => (
-          <div key={tokenName}>
-            <label>
-              {tokenName}
-              <input
-                {...register(tokenName as Path<ProvidersSchema[TProvider]>)}
-                type="password"
-                name={tokenName}
+        </AccordionSummary>
+        <AccordionDetails>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <FormControlLabel
+                label={"On / off"}
+                control={<Checkbox {...register("enabled" as Path<ProvidersSchema[TProvider]>)} />}
               />
-            </label>
-          </div>
-        ))}
-        <button disabled={isLoading} type="submit">
-          {isLoading ? "..." : "Save"}
-        </button>
-      </details>
+            </Grid>
+            {fields.map((tokenName) => (
+              <Grid xs={12} item key={tokenName}>
+                <TextField
+                  {...register(tokenName as Path<ProvidersSchema[TProvider]>)}
+                  label={tokenName}
+                  type="password"
+                  name={tokenName}
+                  fullWidth
+                />
+              </Grid>
+            ))}
+            <Grid item xs={2}>
+              <Button variant="primary" fullWidth disabled={isLoading} type="submit">
+                {isLoading ? "..." : "Save"}
+              </Button>
+            </Grid>
+          </Grid>
+        </AccordionDetails>
+      </Accordion>
     </form>
   );
 };
